7e3a4afdc162fd75cfbddd1539506d45ec2cdbbc
Alex Ehlke <alex.ehlke@gmail.com>
2023-07-19 16:14:11 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftGit2",
-            dependencies: ["Clibgit2", "ZipArchive"]
+            dependencies: ["Clibgit2"]
         )
     ]
 )
