9fedb35eaddffd24d92a1f29653771751927f273
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-28 13:30:36 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -25,9 +25,9 @@ let package = Package(
         .package(url: "https://github.com/lake-of-fire/libgit2-apple.git", branch: "main"),
 //        .package(url: "https://github.com/lake-of-fire/static-libgit2.git", branch: "main"),
         .package(url: "https://github.com/lake-of-fire/LibGit2-On-iOS-thebaselab.git", branch: "master"),
-//        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", branch: "main"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", branch: "main"),
 //        .package(url: "https://github.com/DimaRU/Libssh2Prebuild.git", branch: "master"),
-//        .package(url: "https://github.com/mfcollins3/libssh2-apple.git", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://github.com/mfcollins3/libssh2-apple.git", .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
 //        .binaryTarget(
@@ -77,7 +77,7 @@ let package = Package(
 //                .product(name: "libgit2", package: "libgit2-apple"),
 //                .product(name: "OpenSSL", package: "OpenSSL"),
 //                .product(name: "CSSH", package: "Libssh2Prebuild"),
-//                .product(name: "SSH2", package: "libssh2-apple"),
+                .product(name: "SSH2", package: "libssh2-apple"),
 //                .product(name: "static-libgit2", package: "static-libgit2"),
             ]//,
 //            linkerSettings: [
